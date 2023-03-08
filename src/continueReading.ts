c5b4d83f71c5078286b85af96058b57352be012c
Lucas Diniz <lucasdiniz.smt@gmail.com>
2023-03-08 13:29:07 -0300

    Continue Reading List

---
+++ src/continueReading.ts
@@ -0,0 +1,16 @@
+export const readingList = [
+  {
+    "id": 30054,
+    "title": "Yu-Gi-Oh!",
+    "chapter": "102", 
+    "pageUrl": "https://static.wikia.nocookie.net/yugioh/images/0/0f/YGODDuel102.jpg",
+    "progress": 46
+  },
+  {
+    "id": 30051,
+    "title": "Slam Dunk",
+    "chapter": "26", 
+    "pageUrl": "https://static.wikia.nocookie.net/slamdunk/images/d/d4/Chapter_26.png",
+    "progress": 89
+  }
+]
