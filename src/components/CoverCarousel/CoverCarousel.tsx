54718a60fba61644040b70aa3260538bfd6372d0
Lucas Diniz <lucasdiniz.smt@gmail.com>
2023-03-10 15:21:33 -0300

    fix: CoverCarousel autoplay

--- src/components/CoverCarousel/CoverCarousel.tsx
+++ src/components/CoverCarousel/CoverCarousel.tsx
@@ -11,7 +11,7 @@ interface CoverCarouselProps {
 }
 
 export function CoverCarousel({ mangaList }: CoverCarouselProps) {
-	const autoplay = useRef(Autoplay({ delay: 5000 }));
+	const autoplay = useRef(Autoplay({ delay: 3000 }));
 
   const { classes } = useStyles();
 
